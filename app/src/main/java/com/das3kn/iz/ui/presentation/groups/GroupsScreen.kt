4f1c6be630f3f558d41a3f46a333ee1d2b741cf8
Buğra Karagözoğlu <92315186+Das3kn@users.noreply.github.com>
2025-10-11 23:01:15 +0300

    Fix compose icon button colors import

--- app/src/main/java/com/das3kn/iz/ui/presentation/groups/GroupsScreen.kt
+++ app/src/main/java/com/das3kn/iz/ui/presentation/groups/GroupsScreen.kt
@@ -40,6 +40,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedTextField
@@ -163,7 +164,7 @@ fun GroupsScreen(
                         FilledIconButton(
                             onClick = { isCreateGroupOpen = true },
                             shape = CircleShape,
-                            colors = ButtonDefaults.filledIconButtonColors(
+                            colors = IconButtonDefaults.filledIconButtonColors(
                                 containerColor = Color(0xFF7C3AED),
                                 contentColor = Color.White
                             )
